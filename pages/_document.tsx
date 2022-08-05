c8db3a7a2e687b5a47b2803fbdab2236c0cc157c
Iddan Aaronsohn <mail@aniddan.com>
2022-08-05 17:27:44 +0300

    Add Clicky analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -7,6 +8,17 @@ class MyDocument extends Document {
       <body>
         <Main />
         <NextScript />
+        <script async src="//static.getclicky.com/101375866.js"></script>
+        <noscript>
+          <p>
+            <img
+              alt="Clicky"
+              width="1"
+              height="1"
+              src="//in.getclicky.com/101375866ns.gif"
+            />
+          </p>
+        </noscript>
       </body>
     </Html>
   );
